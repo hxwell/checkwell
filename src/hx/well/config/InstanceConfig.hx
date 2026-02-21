23d8f779b36bda2b1be313f18522ffe0292bb3ea
barisyild <fb_bcy@hotmail.com>
2026-02-21 20:09:07 +0300

    enable HTTP/2 support for Undertow server configuration

--- src/hx/well/config/InstanceConfig.hx
+++ src/hx/well/config/InstanceConfig.hx
@@ -1,6 +1,7 @@
 package hx.well.config;
 import hx.well.server.instance.IInstance;
 import hx.well.http.driver.undertow.UndertowInstance;
+import hx.well.http.driver.undertow.UndertowExtern;
 
 class InstanceConfig implements IConfig {
     public function new() {}
@@ -11,6 +12,7 @@ class InstanceConfig implements IConfig {
             .setJksPath("keystore.jks")
             .setStorePassword("password")
             .setKeyPassword("password")
+            .setServerOption(UndertowOptionsExtern.ENABLE_HTTP2, true)
             .setHost("0.0.0.0")
             .setPort(3000)
             .setSsl(true)
